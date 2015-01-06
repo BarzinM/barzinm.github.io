883da8fae6262582dc880e6f51fbb433548ce459
BarzinM <barzin.m@gmail.com>
2015-01-05 19:20:05 -0500

    fixing styles for different devices

--- js/script.js
+++ js/script.js
@@ -4,11 +4,8 @@ $image = $('.something');
 $window.on('scroll', function() {
 	var top = $window.scrollTop();
 
-	if (top < 0 || top > 1500) { return; }
 	$image
-	.css('transform', 'translate3d(0px, '+top/2+'px, 0px)')
-	.css('opacity', 1-Math.max(top/700, 0));
-	var $color=255-parseInt(top/2);
-
+	.css('transform', 'translate3d(0px, '+top+'px, 0px)')
+	.css('opacity', 1-(top/400));
 });
 $window.trigger('scroll');
