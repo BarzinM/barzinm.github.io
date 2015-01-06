ff3ffc7499d1a053684653d84a631471d65c1198
BarzinM <barzin.m@gmail.com>
2015-01-06 12:38:13 -0500

    removed header opacity script

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ var $window = $(window),
 $image = $('.something');
 $window.on('scroll', function() {
 	var top = $window.scrollTop();
-
-	$image
-	.css('transform', 'translate3d(0px, '+top+'px, 0px)')
-	.css('opacity', 1-(top/400));
+	$image.css('transform', 'translate3d(0px, '+top+'px, 0px)')
 });
 $window.trigger('scroll');
