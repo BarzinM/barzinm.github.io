761ae26ee57f6814ef93d2bcdf45660321b27773
BarzinM <barzin.m@gmail.com>
2015-01-06 19:00:00 -0500

    removed scroll script and kept the effect using css

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(".navbar-brand.page-scroll").hide();
-var $window = $(window),
-$image = $('.something');
-$window.on('scroll', function() {
-	var top = $window.scrollTop();
-	$image.css('transform', 'translate3d(0px, '+top+'px, 0px)')
-});
-$window.trigger('scroll');
