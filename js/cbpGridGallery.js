dd4fd38a9480c2191b59536465377f24c17232fe
BarzinM <barzin.m@gmail.com>
2015-01-08 17:37:13 -0500

    some formating, reducing margin in grid figure class

--- js/cbpGridGallery.js
+++ js/cbpGridGallery.js
@@ -4,28 +4,30 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2014, Codrops
  * http://www.codrops.com
  */
-;( function( window ) {
-	'use strict';
-	function CBPGridGallery( el, options ) {
-		this.el = el;
-  		this._init();
-	}
-	CBPGridGallery.prototype._init = function() {
-		this.grid = this.el.querySelector( 'section.grid-wrap > ul.grid' );
-		this._initMasonry();
-	};
-	CBPGridGallery.prototype._initMasonry = function() {
-		var grid = this.grid;
-		imagesLoaded( grid, function() {
-			new Masonry( grid, {
-				itemSelector: 'li',
-				columnWidth: grid.querySelector( '.grid-sizer' )
-			});
-		});
-	};
-	window.CBPGridGallery = CBPGridGallery;
-})( window );
+;
+(function(window) {
+    'use strict';
+
+    function CBPGridGallery(el, options) {
+        this.el = el;
+        this._init();
+    }
+    CBPGridGallery.prototype._init = function() {
+        this.grid = this.el.querySelector('section.grid-wrap > ul.grid');
+        this._initMasonry();
+    };
+    CBPGridGallery.prototype._initMasonry = function() {
+        var grid = this.grid;
+        imagesLoaded(grid, function() {
+            new Masonry(grid, {
+                itemSelector: 'li',
+                columnWidth: grid.querySelector('.grid-sizer')
+            });
+        });
+    };
+    window.CBPGridGallery = CBPGridGallery;
+})(window);
