a1bc671f01a28fc7a61b2d938f8344cdc97256f7
BarzinM <barzin.m@gmail.com>
2016-04-27 20:44:01 -0400

    made changes to start minimal

---
+++ build/serve/js/cbpAnimatedHeader.js
@@ -0,0 +1,45 @@
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (function() {
+    $(".navbar-brand.page-scroll").hide();
+    var docElem = document.documentElement,
+        header = document.querySelector('.navbar-fixed-top'),
+        didScroll = false,
+        changeHeaderOn = $("#Interests").position().top - 80; // can have -80
+
+    function init() {
+        window.addEventListener('scroll', function(event) {
+            if (!didScroll) {
+                didScroll = true;
+                setTimeout(scrollPage, 250);
+            }
+        }, false);
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if (sy >= changeHeaderOn) {
+            $(".navbar-brand.page-scroll").show(true);
+            classie.add(header, 'navbar-shrink');
+        } else {
+            $(".navbar-brand.page-scroll").hide(true);
+            classie.remove(header, 'navbar-shrink');
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
