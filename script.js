157ef80b44363a1f2622f8f3e06bc655ff4192c8
BarzinM <barzin.m@gmail.com>
2018-11-28 20:40:07 -0500

    made the project demos observable

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-    $(".project_link").click(function () {
-    	// $(this).parent().parent().next().toggleClass("hidden unhidden");
-    	$(this).parent().parent().next().toggleClass("hide show");
+    $(".project_link").click(function () {    	
+    	if ($(this).text() == "show")
+    	       $(this).text("hide");
+    	    else
+    	       $(this).text("show");
+
+    	next=$(this).parent().parent().next()
+    	$(next).find('img').map(function(){
+	    	$(this).attr('src',$(this).data('src'));
+    	})
+
+    	$(next).toggleClass("hide show");
     });
 });
