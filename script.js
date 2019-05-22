054b9cf6d6fb2ab6d577b93a59a2e1e3a9090de5
BarzinM <barzin.m@gmail.com>
2019-05-21 23:10:12 -0700

    message sent function on statusCode success and renabled googleanalytics

--- script.js
+++ script.js
@@ -21,18 +21,17 @@ $(document).ready(function () {
             dataType: "jsonp",
             statusCode: {
                 0: function() {
-                    //Success message
+                    $('#contact_message').val("");
+                    $('#contact_message').attr("placeholder", "Message Sent!");
                 },
                 200: function() {
-                    //Success Message
-                }
+                    $('#contact_message').val("");
+                    $('#contact_message').attr("placeholder", "Message Sent!");                }
             },
             url: "https://docs.google.com/forms/d/e/1FAIpQLSdOvmaG3ca38EO_u2Z61k2m03tv6cBHWZiRBYe-dK5ZP_w8Dw/formResponse",
             data: {
                 'entry.1114812678': $('#contact_message').val()
             }
         });
-        $('#contact_message').val("");
-        $('#contact_message').attr("placeholder", "Message Sent!");
     })
 });
